b523751f74e31800009f89dfd4e003a202069be5
zbastos <josediogobastos@gmail.com>
2016-04-29 16:55:12 +0100

    Loja adicionado

--- Loja.java
+++ Loja.java
@@ -5,29 +5,109 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Loja
+public class Loja extends Imovel
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
+    private Apartamento parteHabitacional;
+    private String tipoNegocio;
+    private double area;
+    private int porta;
+    private boolean WC;
     /**
-     * Constructor for objects of class Loja
+     * Construtor padrão
+     */ 
+    public Loja(){
+       super();
+       this.parteHabitacional = new Apartamento();
+       this.tipoNegocio = "n/a";
+       this.area = 0;
+       this.porta = 0;
+       this.WC = false;
+       }
+    /**
+     * Construtor por parâmatros
+     * @param rua
+     * @param precoMinimo
+     * @param precoPedido
+     * @param parteHab
+     * @param tipo
+     * @param area
+     * @param porta
+     * @param WC
      */
-    public Loja()
-    {
-        // initialise instance variables
-        x = 0;
+    public Loja(String rua, double precoMinimo, double precoPedido, String tipo, double area, int porta, boolean WC){
+       super(rua, precoMinimo, precoPedido);
+       this.tipoNegocio = tipo;
+       this.area = area;
+       this.porta = porta;
+       this.WC = WC;
+       }
+    /**
+     * Construtor por cópia
+     */
+       public Loja(Loja t){
+       super.setRua(t.getRua());
+       super.setPrecoMinimo(t.getPrecoMinimo());
+       super.setPrecoPedido(t.getPrecoPedido());
+       this.tipoNegocio = t.getTipoNegocio();
+       this.area = t.getArea();
+       this.porta = t.getPorta();
+       this.WC = t.getWC();
     }
-
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * Obter tipo de negócio da loja
+     * @return
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public String getTipoNegocio() {
+     return this.tipoNegocio;
+        }
+    /**
+     * Obter área total
+     * @return
+     */
+    public double getArea(){
+    return this.area;
+    }
+    /**
+     * Obter número da porta
+     * @return
+     */
+    public int getPorta(){
+    return this.porta;
+    }
+    /**
+     * Determina se o apartamento tem WC
+     * @return
+     */
+    public boolean getWC(){
+    return this.WC;
+    }
+    /**
+     * Define tipo de negócio 
+     * @param tipo
+     */
+    public void setTipo(String tipo) {
+        this.tipoNegocio = tipoNegocio;
+    }
+    /**
+     * Define área da loja
+     * @param area
+     */
+    public void setArea(double area) {
+        this.area= area;
+    }
+    /**
+     * Define número da porta
+     * @param porta
+     */
+    public void setPorta(int porta) {
+        this.porta = porta;
+    }
+    /**
+     * Define se o apartamento tem, ou não, garagem
+     * @param garagem
+     */
+    public void setWC(boolean WC) {
+        this.WC = WC;
     }
 }
+
