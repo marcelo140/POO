f8ed1b3065d349b24c3f381275db67c6c951961d
zbastos <josediogobastos@gmail.com>
2016-04-29 03:27:02 +0100

    Apartamento adicionado (não terminado)

--- Apartamento.java
+++ Apartamento.java
@@ -15,6 +15,9 @@ public class Apartamento extends Imovel
     private int porta;
     private boolean garagem;
     
+    /**
+     * Construtor padrão
+     */	
     public Apartamento(){
        super();
        this.tipo = "indisponível";
@@ -25,7 +28,20 @@ public class Apartamento extends Imovel
        this.porta = 0;
        this.garagem = false;
        }
-    public Apartamento(String rua, double precoMinimo, double precoPedido, String tipo, double area, private int nQuartos,int nWC, int andar, int porta, boolean garagem){
+    /**
+     * Construtor por parâmatros
+     * @param rua
+     * @param precoMinimo
+     * @param precoPedido
+     * @param tipo
+     * @param area
+     * @param nQuartos
+     * @param nWC
+     * @param andar
+     * @param porta
+     * @param garagem
+     */
+    public Apartamento(String rua, double precoMinimo, double precoPedido, String tipo, double area, int nQuartos,int nWC, int andar, int porta, boolean garagem){
        super(rua, precoMinimo, precoPedido);
        this.tipo = tipo;
        this.area = area;
@@ -35,17 +51,118 @@ public class Apartamento extends Imovel
        this.porta = porta;
        this.garagem = garagem;
        }
+    /**
+     * Construtor por cópia
+     */
        public Apartamento(Apartamento t){
-		super.setRua(t.getRua());
-		super.setPrecoMinimo(t.getPrecoMinimo());
-		super.setPrecoPedido(t.getPrecoPedido());
-		this.tipo = tipo;
-       this.area = area;
-       this.nQuartos = nQuartos;
-       this.nWC = nWC;
-       this.andar = andar;
-       this.porta = porta;
-       this.garagem = garagem;
+	   super.setRua(t.getRua());
+	   super.setPrecoMinimo(t.getPrecoMinimo());
+	   super.setPrecoPedido(t.getPrecoPedido());
+	   this.tipo = t.getTipo();
+       this.area = t.getArea();
+       this.nQuartos = t.getNQuartos();
+       this.nWC = t.getNWC();
+       this.andar = t.getAndar();
+       this.porta = t.getPorta();
+       this.garagem = t.getGaragem();
 	}
+	/**
+     * Obter tipo de apartamento
+     * @return
+     */
+    public String getTipo() {
+	 return this.tipo;
         }
+    /**
+     * Obter área total
+     * @return
+     */
+    public double getArea(){
+    return this.area;
+    }
+    /**
+     * Obter número de quartos
+     * @return
+     */
+    public int getNQuartos(){
+    return this.nQuartos;
+    }
+    /**
+     * Obter números de WCs
+     * @return
+     */
+    public int getNWC(){
+    return this.nWC;
+    }
+    /**
+     * Obter andar
+     * @return
+     */
+    public int getAndar(){
+    return this.andar;
+    }
+    /**
+     * Obter número da porta
+     * @return
+     */
+    public int getPorta(){
+    return this.porta;
+    }
+    /**
+     * Determina se o apartamento tem garagem
+     * @return
+     */
+    public boolean getGaragem(){
+    return this.garagem;
+    }
+    /**
+     * Define tipo de apartamento
+     * @param tipo
+     */
+    public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	/**
+     * Define área do apartamento
+     * @param area
+     */
+	public void setArea(double area) {
+		this.area= area;
+	}
+	/**
+     * Define número de quartos
+     * @param NQuartos
+     */
+	public void setNQuartos(int nQuartos) {
+		this.nQuartos = nQuartos;
+	}
+	/**
+     * Define número de WCs
+     * @param nWC
+     */
+	public void setNWC(int nWC) {
+		this.nWC = nWC;
+	}
+	/**
+     * Define andar
+     * @param andar
+     */
+	public void setAndar(int andar) {
+		this.andar = andar;
+	}
+	/**
+     * Define número da porta
+     * @param porta
+     */
+	public void setPorta(int porta) {
+		this.porta = porta;
+	}
+	/**
+     * Define se o apartamento tem, ou não, garagem
+     * @param garagem
+     */
+	public void setGaragem(boolean garagem) {
+		this.garagem = garagem;
+	}
+}
 
